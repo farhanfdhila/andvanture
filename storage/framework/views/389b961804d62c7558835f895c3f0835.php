720e969bc960a7ab0a928c44433c56075f6390d2
farhanfdhila <parhanpaadilah@gmail.com>
2026-05-22 11:27:52 +0700

    Hard reset cache biar nggak pingsan

--- storage/framework/views/389b961804d62c7558835f895c3f0835.php
+++ storage/framework/views/389b961804d62c7558835f895c3f0835.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Farhan's Travel & Hike Memories</title>
+    <title>Andvanturee</title>
     <?php echo app('Illuminate\Foundation\Vite')('resources/css/app.css'); ?>
     <link href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@400;700&display=swap" rel="stylesheet">
     <style> body { font-family: 'Space Grotesk', sans-serif; } </style>
