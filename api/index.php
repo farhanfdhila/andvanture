b8e17a75e2976d81a4c548a29db280bbdf3ef456
farhanfdhila <parhanpaadilah@gmail.com>
2026-05-29 18:47:38 +0700

    migrate to static HTML for Vercel

--- api/index.php
+++ api/index.php
@@ -1,3 +1,9 @@
 <?php
-// Forward Vercel requests to normal Laravel public/index.php
+// api/index.php
+$_ENV['APP_ENV'] = 'production';
+$_ENV['APP_DEBUG'] = 'false';
+$_ENV['CACHE_DRIVER'] = 'array';
+$_ENV['VIEW_COMPILED_PATH'] = '/tmp';
+$_ENV['SESSION_DRIVER'] = 'cookie';
+
 require __DIR__ . '/../public/index.php';
